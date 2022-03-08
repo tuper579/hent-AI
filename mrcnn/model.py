ec2ffd05a703f49b42bd94c91c1bcaa127cde011
tuper579 <58165592+tuper579@users.noreply.github.com>
2022-03-08 16:36:03 +0800

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -20,7 +20,8 @@ import tensorflow as tf
 import keras
 import keras.backend as K
 import keras.layers as KL
-import keras.engine.topology as KE
+import keras.engine as KE
+from keras.layers import Layer
 import keras.models as KM
 
 from mrcnn import utils
